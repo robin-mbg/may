bf1bc30a08e757d9987d388aab4de8d1758461d4
Robin Meiseberg <robin.meiseberg@external.flixbus.com>
2020-03-28 19:18:19 +0100

    Extract repository finding, add basic type detection

---
+++ find/find.go
@@ -0,0 +1,111 @@
+package find
+
+import (
+    "os"
+    "fmt"
+    "strings"
+    "github.com/cheggaaa/pb/v3"
+    "path/filepath"
+)
+
+var (
+    bar          *pb.ProgressBar
+    targetFolder string
+    targetFile   string
+    gitRepositoriesList []string
+)
+
+func FindCandidate(name string) string {
+    // List all available repositories
+    basepath := os.Getenv("HOME")
+    listGitDirectories(basepath)
+
+    candidates := []string{}
+
+    // Find candidates for path
+    for _, v := range gitRepositoriesList {
+        if strings.HasSuffix(v, name) {
+          fmt.Println("Found a match:", v)
+          candidates = append(candidates, v)
+        }
+    }
+
+    if len(candidates) > 1 {
+        fmt.Println("Found more than one match")
+        os.Exit(1)
+    }
+
+    finalCandidate := candidates[0]
+
+    return finalCandidate
+}
+
+func listGitDirectories(basepath string) {
+    count := 10000
+
+    tmpl := `{{ green "Searching for" }} {{string . "path_string" | blue}} {{ bar . "<" "-" (cycle . "↖" "↗" "↘" "↙" ) "." ">"}}`
+    bar = pb.ProgressBarTemplate(tmpl).Start(count)
+    bar.Set("path_string", "git repositories")
+
+    targetFolder = basepath
+    targetFile = ".git"
+
+    // sanity check
+    testFile, err := os.Open(targetFolder)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(-1)
+    }
+    defer testFile.Close()
+
+    testFileInfo, _ := testFile.Stat()
+    if !testFileInfo.IsDir() {
+        fmt.Println(targetFolder, " is not a directory!")
+        os.Exit(-1)
+    }
+
+    err = filepath.Walk(targetFolder, findGitRepository)
+    bar.Finish()
+
+    fmt.Println("Found", len(gitRepositoriesList), "git repositories.")
+}
+
+func findGitRepository(path string, fileInfo os.FileInfo, err error) error {
+         bar.Increment()
+         if err != nil {
+                 fmt.Println(err)
+                 return nil
+         }
+
+         absolute, err := filepath.Abs(path)
+
+         if err != nil {
+                 fmt.Println(err)
+                 return nil
+         }
+
+         if fileInfo.IsDir() {
+                 // correct permission to scan folder?
+                 testDir, err := os.Open(absolute)
+
+                 if err != nil {
+                         if os.IsPermission(err) {
+                                 fmt.Println("No permission to scan ... ", absolute)
+                                 fmt.Println(err)
+                         }
+                 }
+                 matched, err := filepath.Match(targetFile, fileInfo.Name())
+                 if err != nil {
+                         fmt.Println(err)
+                 }
+
+                 if matched {
+                         add := absolute
+                         gitRepositoriesList = append(gitRepositoriesList, strings.TrimSuffix(add, "/.git"))
+                         testDir.Close()
+                         return nil
+                }
+          }
+          return nil
+ }
+
--- inspect/inspect.go
+++ inspect/inspect.go
@@ -1,112 +1,86 @@
 package inspect
 
 import (
-    "os"
     "fmt"
-    "strings"
-    "github.com/cheggaaa/pb/v3"
-    "path/filepath"
-)
-
-var (
-    bar          *pb.ProgressBar
-    targetFolder string
-    targetFile   string
-    gitRepositoriesList []string
+    "os"
+    "os/exec"
+    "github.com/robin-mbg/may/find"
 )
 
 func RunInspection(name string) {
-    // List all available repositories
-    basepath := os.Getenv("HOME")
-    listGitDirectories(basepath)
+    // Find candidate
+    path := find.FindCandidate(name)
 
-    candidates := []string{}
+    // Run inspection
+    fmt.Println("Running inspection on", path)
 
-    // Find candidates for path
-    for _, v := range gitRepositoriesList {
-        if strings.HasSuffix(v, name) {
-          fmt.Println("Found a match:", v)
-          candidates = append(candidates, v)
+    if isGradleProject(path) {
+        RunCommand(path + "/gradlew", "tasks", path)
+        os.Exit(0)
+    }
+
+    if isYarnProject(path) {
+        RunCommand("yarn", "run", path)
+        os.Exit(0)
+    }
+
+    if isGoProject(path) {
+        RunCommand("go", "help", path)
+        os.Exit(0)
+    }
+}
+
+func RunCommand(path string, argument string, dir string) {
+    cmd := exec.Command(path, argument)
+    cmd.Dir = dir
+
+    out, err := cmd.Output()
+
+    if err != nil {
+        fmt.Printf("%s", err)
+        fmt.Println()
+    }
+
+    output := string(out[:])
+    fmt.Println(output)
+}
+
+func isGradleProject(path string) bool {
+    testPath := path + "/gradlew"
+
+    if exists(testPath) {
+        fmt.Println("Specified project is of type `gradle`")
+        return true
+    }
+    return false
+}
+
+func isYarnProject(path string) bool {
+    testPath := path + "/yarn.lock"
+
+    if exists(testPath) {
+        fmt.Println("Specified project is of type `yarn`")
+        return true
+    }
+    return false
+}
+
+func isGoProject(path string) bool {
+     testPath := path + "/go.mod"
+
+    if exists(testPath) {
+        fmt.Println("Specified project is of type `golang`")
+        return true
+    }
+    return false
+}
+
+// Exists reports whether the named file or directory exists.
+func exists(path string) bool {
+    if _, err := os.Stat(path); err != nil {
+        if os.IsNotExist(err) {
+            return false
         }
     }
-
-    if len(candidates) > 1 {
-        fmt.Println("Found more than one match")
-        os.Exit(1)
-    }
-
-    finalCandidate := candidates[0]
-
-    // Run inspection on most promising candidate
-    fmt.Println("Running inspection on", finalCandidate)
+    return true
 }
-
-func listGitDirectories(basepath string) {
-    count := 10000
-
-    tmpl := `{{ green "Searching for" }} {{string . "path_string" | blue}} {{ bar . "<" "-" (cycle . "↖" "↗" "↘" "↙" ) "." ">"}}`
-    bar = pb.ProgressBarTemplate(tmpl).Start(count)
-    bar.Set("path_string", "git repositories")
-
-    targetFolder = basepath
-    targetFile = ".git"
-
-    // sanity check
-    testFile, err := os.Open(targetFolder)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(-1)
-    }
-    defer testFile.Close()
-
-    testFileInfo, _ := testFile.Stat()
-    if !testFileInfo.IsDir() {
-        fmt.Println(targetFolder, " is not a directory!")
-        os.Exit(-1)
-    }
-
-    err = filepath.Walk(targetFolder, findGitRepository)
-    bar.Finish()
-
-    fmt.Println("Found", len(gitRepositoriesList), "git repositories.")
-}
-
-func findGitRepository(path string, fileInfo os.FileInfo, err error) error {
-         bar.Increment()
-         if err != nil {
-                 fmt.Println(err)
-                 return nil
-         }
-
-         absolute, err := filepath.Abs(path)
-
-         if err != nil {
-                 fmt.Println(err)
-                 return nil
-         }
-
-         if fileInfo.IsDir() {
-                 // correct permission to scan folder?
-                 testDir, err := os.Open(absolute)
-
-                 if err != nil {
-                         if os.IsPermission(err) {
-                                 fmt.Println("No permission to scan ... ", absolute)
-                                 fmt.Println(err)
-                         }
-                 }
-                 matched, err := filepath.Match(targetFile, fileInfo.Name())
-                 if err != nil {
-                         fmt.Println(err)
-                 }
-
-                 if matched {
-                         add := absolute
-                         gitRepositoriesList = append(gitRepositoriesList, strings.TrimSuffix(add, "/.git"))
-                         testDir.Close()
-                         return nil
-                }
-          }
-          return nil
- }
-
