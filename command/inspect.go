e4243218ca674822ca4326e03cb48f6f88b9bed2
Robin Meiseberg <robin.meiseberg@external.flixbus.com>
2020-04-04 17:06:45 +0200

    Refactor inspection code

--- command/inspect.go
+++ command/inspect.go
@@ -6,32 +6,10 @@ import (
 )
 
 // Inspect takes a repository name parameter and shows which build tool `may` would use to run commands on that repository.
-// It also calls on that tool to print available commands.
 func Inspect(path string) {
 	util.Log("Running inspection on " + path)
 
-	var preferredExecutor = ""
-
-	if isDockerProject(path) {
-		preferredExecutor = "docker"
-	}
-	if isGoProject(path) {
-		preferredExecutor = "go"
-	}
-	if isYarnProject(path) {
-		preferredExecutor = "yarn"
-	}
-	if isGradleProject(path) {
-		preferredExecutor = "gradle"
-	}
-	if isMakefileProject(path) {
-		preferredExecutor = "make"
-	}
-
-	if preferredExecutor == "" {
-		util.LogError("No executor could be determined. Consider adding a Makefile.")
-		return
-	}
+	preferredExecutor := GetExecutor(path)
 
 	util.LogDebug("Commands on this project would be run using `" + preferredExecutor + "`.")
 }
@@ -39,32 +17,39 @@ func Inspect(path string) {
 // GetExecutor takes a file system paths and prints which build tool it would
 // use to execute commands for that path
 func GetExecutor(path string) string {
-	if isMakefileProject(path) {
-		return "make"
-	}
-	if isGradleProject(path) {
-		return path + "/gradlew"
-	}
-	if isYarnProject(path) {
-		return "yarn"
+	var possibleExecutors = []string{}
+
+	if isDockerProject(path) {
+		possibleExecutors = append(possibleExecutors, "docker")
 	}
 	if isGoProject(path) {
-		return "go"
+		possibleExecutors = append(possibleExecutors, "go")
 	}
-	if isDockerProject(path) {
-		return "docker"
+	if isYarnProject(path) {
+		possibleExecutors = append(possibleExecutors, "yarn")
+	}
+	if isGradleProject(path) {
+		possibleExecutors = append(possibleExecutors, path+"/gradlew")
+	}
+	if isMakefileProject(path) {
+		possibleExecutors = append(possibleExecutors, "make")
 	}
 
-	util.LogError("No executor could be detected for project")
-	os.Exit(1)
-	return ""
+	if len(possibleExecutors) == 0 {
+		util.LogError("No executor could be determined. Consider adding a Makefile.")
+		return ""
+	}
+
+	preferredExecutor := possibleExecutors[len(possibleExecutors)-1]
+
+	return preferredExecutor
 }
 
 func isMakefileProject(path string) bool {
 	testPath := path + "/Makefile"
 
 	if exists(testPath) {
-		util.LogDebug(path + " has a Makefile")
+		util.LogDebug(path + " can be run using `make`")
 		return true
 	}
 	return false
@@ -74,7 +59,7 @@ func isGradleProject(path string) bool {
 	testPath := path + "/gradlew"
 
 	if exists(testPath) {
-		util.LogDebug(path + " is of type `gradle`")
+		util.LogDebug(path + " can be run using `gradle`")
 		return true
 	}
 	return false
@@ -84,7 +69,7 @@ func isYarnProject(path string) bool {
 	testPath := path + "/yarn.lock"
 
 	if exists(testPath) {
-		util.LogDebug(path + " is of type `yarn`")
+		util.LogDebug(path + " can be run using `yarn`")
 		return true
 	}
 	return false
@@ -94,7 +79,7 @@ func isGoProject(path string) bool {
 	testPath := path + "/go.mod"
 
 	if exists(testPath) {
-		util.LogDebug(path + " is of type `golang`")
+		util.LogDebug(path + " can be run using `go`")
 		return true
 	}
 	return false
@@ -104,7 +89,7 @@ func isDockerProject(path string) bool {
 	testPath := path + "/Dockerfile"
 
 	if exists(testPath) {
-		util.LogDebug(path + " is of type `docker`")
+		util.LogDebug(path + " can be run using `docker`")
 		return true
 	}
 	return false
