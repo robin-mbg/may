2be1c55b3f802e736bb849cbb2d2f48b60152dc2
Robin Meiseberg <robin.meiseberg@external.flixbus.com>
2020-04-04 17:26:44 +0200

    Enable shell pipes based on output

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cheggaaa/pb/v3 v3.0.4
 	github.com/gookit/color v1.2.3
 	github.com/spf13/pflag v1.0.5
+	golang.org/x/crypto v0.0.0-20200403201458-baeed622b8d8
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/tools v0.0.0-20200403190813-44a64ad78b9b // indirect
 )
--- util/log.go
+++ util/log.go
@@ -1,30 +1,56 @@
 package util
 
 import (
+	"fmt"
 	"github.com/gookit/color"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
 )
 
 // Log prints a default log message.
 func Log(message string) {
-	color.Green.Println(message)
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		color.Green.Println(message)
+	} else {
+		LogRaw(message)
+	}
 }
 
 // LogDebug prints a debug-level log message.
 func LogDebug(message string) {
-	color.White.Println(message)
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		color.White.Println(message)
+	} else {
+		LogRaw(message)
+	}
 }
 
 // LogImportant prints an important-level log message.
 func LogImportant(message string) {
-	color.Magenta.Println(message)
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		color.Magenta.Println(message)
+	} else {
+		LogRaw(message)
+	}
 }
 
 // LogError prints an error-level log message.
 func LogError(message string) {
-	color.Red.Println(message)
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		color.Red.Println(message)
+	} else {
+		LogRaw(message)
+	}
 }
 
 // LogSeparator prints a visual separator.
 func LogSeparator() {
-	color.Green.Println("─────────────────────")
+	if terminal.IsTerminal(int(os.Stdout.Fd())) {
+		color.Green.Println("─────────────────────")
+	}
+}
+
+// LogRaw prints a non-colored log message.
+func LogRaw(message string) {
+	fmt.Println(message)
 }
